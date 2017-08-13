3d3705c7c293435caca0e36aa5b7548d43eef6b8
Todsaporn Sangboon <nolifelover@gmail.com>
2017-08-14 05:06:51 +0700

    bootstrap data

--- grails-app/init/grails/demo/BootStrap.groovy
+++ grails-app/init/grails/demo/BootStrap.groovy
@@ -1,8 +1,31 @@
 package grails.demo
-
+import com.twitter.*;
 class BootStrap {
 
     def init = { servletContext ->
+    	def johnUser = new User(username:"john_doe", fullname: "John Doe", text:"my name is Jonh Doe, Nice to meet u :)")
+    	def janeUser = new User(username:"jane_doe", fullname: "Jane Doe", text:"Hello world, Nict to meet u")
+
+    	johnUser.save()
+    	janeUser.save()
+
+    	def helloTag = new Tag(text: "hello")
+    	def worldTag = new Tag(text: "world")
+
+    	helloTag.save()
+    	worldTag.save()
+
+    	def johnTweet = new Tweet(text:"Hello Universe from world #hello #world", user: johnUser)
+
+    	johnTweet.save()
+
+    	def johnTweetHelloTag = new TweetTag(tweet: johnTweet, tag:helloTag)
+    	johnTweetHelloTag.save()
+
+    	johnTweet.addToTweetTags(tag: worldTag)
+
+    	johnTweet.save()
+
     }
     def destroy = {
     }
