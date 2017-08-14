802c68c0c9d766060293b9f894c533dc145d4682
Todsaporn Sangboon <nolifelover@gmail.com>
2017-08-15 00:37:48 +0700

    add tweet taglib

---
+++ grails-app/taglib/com/twitter/TweetHelperTagLib.groovy
@@ -0,0 +1,6 @@
+package com.twitter
+
+class TweetHelperTagLib {
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+}
